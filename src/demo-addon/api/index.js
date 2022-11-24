ae7d9bc471dac5989f9f2827be0536e31373da2c
Kubessandra <aymeric.beudaert@gmail.com>
2022-11-24 12:11:51 +0100

    feat(auth): validate token when syncing the storybook
    .

--- src/demo-addon/api/index.js
+++ src/demo-addon/api/index.js
@@ -1,10 +1,32 @@
 const ANIMA_API_URL = "https://api.animaapp.com";
+const PREFIX = "/services/s2f";
+
+const SB_SERVICE_URL = `${ANIMA_API_URL}/${PREFIX}`;
+
+export const authenticate = async (token) => {
+  if (!token) throw new Error("No token provided");
+  const res = await fetch(`${SB_SERVICE_URL}/validate_token`, {
+    method: "GET",
+    headers: {
+      Authorization: "Bearer " + token,
+    },
+  });
+  if (res.status === 200) {
+    const data = await res.json();
+    return { success: true, data };
+  }
+  if (res.status > 299) {
+    const json = await res.json();
+    throw new Error(json?.message || "Missing Storybook token");
+  }
+};
 
 export const syncStorybook = async (token) => {
   if (!token) {
     throw new Error("No token in the query params");
   }
-  await fetch(`${ANIMA_API_URL}/storybook/sample`, {
+  await authenticate(token);
+  await fetch(`${SB_SERVICE_URL}/storybook/sample`, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
@@ -12,4 +34,3 @@ export const syncStorybook = async (token) => {
     },
   });
 };
-
