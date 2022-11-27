262143e88c9ceafc37d7d9ea4a6d88ef4aeeb2b2
Amir Lellouche <amir@animaapp.com>
2022-11-27 17:37:28 +0200

    fix: a small change to avoide hitting cache

--- src/stories/Avatar.stories.jsx
+++ src/stories/Avatar.stories.jsx
@@ -13,7 +13,7 @@ export const MuiAvatar = Template.bind({});
 
 MuiAvatar.args = {
   src: 'https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80',
-  letter: 'A',
+  letter: 'B',
   bgColor: 'red',
   lettered: false,
 };
